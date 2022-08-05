53866394616214e1c2c288141f1811914a168da3
JssKaph <tntmd2658@gmail.com>
2022-08-05 14:10:48 +0900

    0805 오전수업

--- src/Derived.java
+++ src/Derived.java
@@ -5,8 +5,10 @@ public class Derived extends Base {
 		System.out.println("나는 자식의 메소드"); 
 	}
 	void display() { // 두명의 이름을 가져오고 싶을때
-		System.out.println(this.name); // this.name = 나의 이름
-		System.out.println(super.name); // super.name = 부모의 이름
+//		System.out.println(this.name); // this.name = 나의 이름
+//		System.out.println(super.name); // super.name = 부모의 이름
+		this.print();
+		super.print();
 	}
 }
 
